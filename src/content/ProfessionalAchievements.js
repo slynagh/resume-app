1fdad71951a71eee171349f77858010696322497
MSI\steve <stevelynagh@hotmail.com>
2019-06-27 11:49:09 +0200

    added Professional Achievements content added local workspace settings file

---
+++ src/content/ProfessionalAchievements.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <React.Fragment>
+            <p>Advocacy and education of source control tools and modular project structures, leading to their adoption.</p>
+            <p>Design, development and maintenance of numerous ACRRM apps based on Phonegap, Adobe AIR and 3rd party app generator (DoubleDutch)</p>
+            <p>Design and development of Keynote digital assets including interactive projection background "mini-app" for 14th World Rural Health Conference, 2016, attending by 1400 delegates</p>
+            <p>Developed bespoke minisite for ACRRM's AGPT training program.</p>
+        </React.Fragment>
+    )
+}
