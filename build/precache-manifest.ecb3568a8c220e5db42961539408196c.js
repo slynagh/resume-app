584dae004cff51bff2ee5c5b875e1e83892f0b06
MSI\steve <stevelynagh@hotmail.com>
2019-07-10 11:05:01 +0200

    build 20190710

---
+++ build/precache-manifest.ecb3568a8c220e5db42961539408196c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7c3d9c7512f34c19e54338e2c0ad2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa9252cd716fb6235165",
+    "url": "/static/css/main.b05c1635.chunk.css"
+  },
+  {
+    "revision": "b95508c3d7c2154bece2",
+    "url": "/static/js/2.6223ab63.chunk.js"
+  },
+  {
+    "revision": "fa9252cd716fb6235165",
+    "url": "/static/js/main.62433d0f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
