e9daf205b6f431a5393134f9405065b93f29107f
MSI\steve <stevelynagh@hotmail.com>
2025-03-31 14:28:02 +1000

    refactored Content component

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,35 +7,29 @@ import ContentLinks from "../content/Links";
 import ContentReferences from "../content/References";
 import ContentProfessionalAcheivements from "../content/ProfessionalAchievements";
 
-function Content(props) {
-  const MyContent = (function () {
-    switch (props.name) {
-      case "Summary":
-        return Objective;
-      case "ProfessionalAchievements":
-        return ContentProfessionalAcheivements;
-      case "WorkHistory":
-        return ContentWorkHistory;
-      case "Skills":
-        return ContentSkills;
-      case "Education":
-        return ContentEducation;
-      case "PointsOfInterest":
-        return ContentPointsOfInterest;
-      case "Links":
-        return ContentLinks;
-      case "References":
-        return ContentReferences;
-      default:
-        return Objective;
-    }
-  })();
-
-  return (
-    <div {...props}>
-      <MyContent />
-    </div>
-  );
+function getContent(name) {
+  switch (name) {
+    case "Objective":
+      return <Objective />;
+    case "ProfessionalAchievements":
+      return <ContentProfessionalAcheivements />;
+    case "WorkHistory":
+      return <ContentWorkHistory />;
+    case "Skills":
+      return <ContentSkills />;
+    case "Education":
+      return <ContentEducation />;
+    case "PointsOfInterest":
+      return <ContentPointsOfInterest />;
+    case "Links":
+      return <ContentLinks />;
+    case "References":
+      return <ContentReferences />;
+    default:
+      return <Objective />;
+  }
 }
+
+const Content = (props) => <div {...props}>{getContent(props.name)}</div>;
 
 export default Content;
