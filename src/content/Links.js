e219be05c109703cc9648387514b9256b485d461
MSI\steve <stevelynagh@hotmail.com>
2025-04-08 15:13:11 +1000

    updated Links, icomoon

--- src/content/Links.js
+++ src/content/Links.js
@@ -12,86 +12,67 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const data = [
+  // {
+  //   link: "http://stevenlynagh.com",
+  //   icon: "Web",
+  //   fontIcon: null,
+  //   text: "Personal Website",
+  // },
+  {
+    link: "https://www.linkedin.com/in/stevenlynagh/",
+    fontIcon: "icon-linkedin",
+    text: "LinkedIn profile",
+  },
+  {
+    link: "https://github.com/slynagh",
+    fontIcon: "icon-github",
+    text: "GitHub profile",
+  },
+  {
+    link: "https://stackoverflow.com/users/2790882/slynagh?tab=profile",
+    fontIcon: "icon-stackoverflow",
+    text: "Stack Overflow profile",
+  },
+
+  // {
+  //   link: "https://www.freecodecamp.org/stevelynagh",
+  //   fontIcon: "icon-free-code-camp",
+  //   text: "FreeCodeCamp profile",
+  // },
+  // {
+  //   link: "https://app.pluralsight.com/profile/steven-lynagh",
+  //   fontIcon: "icon-pluralsight",
+  //   text: "Pluralsight SkillsIQ",
+  // },
+];
+
 const Links = () => {
   const classes = useStyles();
 
   return (
     <ul className={classes.root}>
-      <li>
-        <Button
-          className={classes.button}
-          color="primary"
-          variant="contained"
-          size="large"
-          component="a"
-          href="http://stevenlynagh.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Icon name="Web" />
-          &nbsp;&nbsp; Personal Website
-        </Button>
-      </li>
-      <li>
-        <Button
-          className={classes.button}
-          color="primary"
-          variant="contained"
-          size="large"
-          component="a"
-          href="https://www.linkedin.com/in/stevenlynagh/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontIcon className="icon-linkedin" />
-          &nbsp;&nbsp; LinkedIn
-        </Button>
-      </li>
-      <li>
-        <Button
-          className={classes.button}
-          color="primary"
-          variant="contained"
-          size="large"
-          component="a"
-          href="https://stackoverflow.com/users/2790882/slynagh?tab=profile"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontIcon className="icon-stackoverflow" />
-          &nbsp;&nbsp; Stack Overflow
-        </Button>
-      </li>
-      <li>
-        <Button
-          className={classes.button}
-          color="primary"
-          variant="contained"
-          size="large"
-          component="a"
-          href="https://github.com/slynagh"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontIcon className="icon-github" />
-          &nbsp;&nbsp; GitHub
-        </Button>
-      </li>
-      <li>
-        <Button
-          className={classes.button}
-          color="primary"
-          variant="contained"
-          size="large"
-          component="a"
-          href="https://www.freecodecamp.org/stevelynagh"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontIcon className="icon-free-code-camp overflow-visible" />{" "}
-          &nbsp;&nbsp; FreeCodeCamp
-        </Button>
-      </li>
+      {data.map((item, index) => (
+        <li key={index}>
+          <Button
+            className={classes.button}
+            color="primary"
+            variant="contained"
+            size="large"
+            component="a"
+            href={item.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {item.fontIcon ? (
+              <FontIcon className={`${item.fontIcon} overflow-visible`} />
+            ) : (
+              <Icon name={item.icon} />
+            )}
+            &nbsp;&nbsp; {item.text}
+          </Button>
+        </li>
+      ))}
     </ul>
   );
 };
